06dcb546826b69f154e601b3ce80d63eb43f0152
Michael Brevig <michaelbrevig@gmail.com>
2015-03-12 14:03:20 -0500

    updated index.html and tools.js - found bug with including fabric.js

--- js/fuji-mats/tools.js
+++ js/fuji-mats/tools.js
@@ -17,11 +17,32 @@ tools = {
             .find('button.active-btn')
             .each(function(){
                 $(this).bind('click', function(){
+                    console.log('clicked');
                     var func = $(this).attr('data-func');
-                    fujiDebug.append('Toolmenu button clicked. Function: '+func);
+                    fujiDebug.append('Tool button clicked. Function: '+func);
+
+                    //var action = tools[func];
+                    //action(this);
+                    if(func == 'settings'){
+                        tools.settings(this);
+                    }
                 });
             });
+    },
+
+    settings: function(obj){
+        var activeDiv = $(obj).parent().parent();
+        var settingsDiv = $('#toolSettings');
+
+
+        activeDiv.after(settingsDiv);
+
     }
+}
 
 
-};
+
+
+
+
+;
