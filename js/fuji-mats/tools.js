a4834228b0bc5dfe75d9e4237370ef18b6e6e4c9
Michael Brevig <michaelbrevig@gmail.com>
2015-03-13 15:24:00 -0500

    added images to /images and updated tools.js

--- js/fuji-mats/tools.js
+++ js/fuji-mats/tools.js
@@ -5,38 +5,68 @@
  * @type {{menu: {}, init: Function, bindActions: Function}}
  */
 tools = {
-
+    _data: {},
     menu: {},
 
-    init: function(){
+    init: function () {
         tools.menu = $('#toolmenu');
     },
 
-    bindActions: function(){
+    bindActions: function () {
         $(tools.menu)
             .find('button.active-btn')
-            .each(function(){
-                $(this).bind('click', function(){
-                    console.log('clicked');
-                    var func = $(this).attr('data-func');
-                    fujiDebug.append('Tool button clicked. Function: '+func);
+            .each(function () {
+                $(this).bind('click', function () {
 
-                    //var action = tools[func];
-                    //action(this);
-                    if(func == 'settings'){
-                        tools.settings(this);
-                    }
+                    var method = $(this).attr('data-action');
+                    fujiDebug.append('Tool button clicked. Method: ' + method);
+
+                    var method2 = $(this).attr('data-condition');
+                    fujiDebug.append('Tool button clicked. Chained 2nd method: ' + method2);
+                    var action = tools[method][method2];
+                    action(this);
+
                 });
             });
     },
 
-    settings: function(obj){
-        var activeDiv = $(obj).parent().parent();
-        var settingsDiv = $('#toolSettings');
+    settings: {
 
+        div:
+            function (obj) {
+                if(tools._data.settingsSet) {
+                    $('#toolSettings').show();
+                    return;
+                }
 
-        activeDiv.after(settingsDiv);
+                var activeDiv = $(obj).parent().parent();
+                var settingsDiv = $('#toolSettings');
 
+                activeDiv.after(settingsDiv);
+
+                $('#save-settings').bind('click', function(e){
+                    tools.settings.save(this);
+
+                    $(this).prop('disabled', true );
+                    e.preventDefault();
+
+                });
+            },
+        save:
+            function (){
+                //this will need a lot of work.
+                tools._data.unitOfMeasure = $('input[name=unitOfMeasure]').val();
+
+                fujiDebug.append('Unit of Measurement saved: ' +tools._data.unitOfMeasure);
+                tools._data.settingsSet = true;
+
+                $('.glyphicon-pencil').removeClass('disabled');
+
+                //start grid
+                grid.setId(gridId);
+                grid.create();
+                $('#toolSettings').hide();
+            }
     }
 }
 
